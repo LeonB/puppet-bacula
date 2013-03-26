49f383ce5caa562b1ef80d4c2ab0cd461135b164
Leon Bogaert <leon@tim-online.nl>
2013-03-26 23:48:28 +0100

    Removed unused code

--- manifests/director/client.pp
+++ manifests/director/client.pp
@@ -7,9 +7,6 @@ define bacula::director::client (
 		fail "Name for client ${name} must be a fully qualified domain name"
 	}
 
-	$name_array = split($name, '[.]')
-	$hostname   = $name_array[0]
-
 	file { "/etc/bacula/bacula-dir.d/${name}.conf":
 		ensure  => file,
 		content => template('bacula/client_config.erb'),
